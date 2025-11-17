fb840fd15106a1806e0f22a4e3a1657b4259807c
karanbk67 <karanbishwokarma67@gmail.com>
2025-11-17 11:03:41 +0545

    Predicting Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,8 +48,38 @@ class ModelTrainer:
                 "CatBoost Regressor":CatBoostRegressor(),
                 "AdaBoost Regressor":AdaBoostRegressor()
             }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbours":{
+                    'n_neighbors':[5,7,9,11]
+                },  
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
 
-            model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models, param=params)
             ## To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
